2d6e15907b94034cc588281bee101e1bf18c5df2
Minh Le <duyminh.le310@gmail.com>
2023-03-11 22:18:19 -0500

    Add sample products for FeaturedProductsSection stories

--- src/components/sections/FeaturedProductsSection/FeaturedProductsSection.stories.tsx
+++ src/components/sections/FeaturedProductsSection/FeaturedProductsSection.stories.tsx
@@ -3,10 +3,75 @@ import {  Meta, StoryObj } from "@storybook/react";
 import FeaturedProductsSection from '.';
 import type { Props } from '.';
 
+
+const productSample = {
+  id: "1",
+  name: "Product Name",
+  price: 100,
+  intro: "This is some intro text. I'm trying to make it longer to see if it fit on the frame",
+  images: [
+    {
+      id: "img-1",
+      src: "https://picsum.photos/seed/picsum/200/200",
+      alt: "Product Image 1"
+    },
+    {
+      id: "img-2",
+      src: "https://picsum.photos/seed/picsum/200/200",
+      alt: "Product Image 2"
+    },
+    {
+      id: "img-3",
+      src: "https://picsum.photos/seed/picsum/200/200",
+      alt: "Product Image 3"
+    }
+  ]
+}
+
+const productSamples = [
+  {
+    ...productSample,
+    id: "1"
+  },
+  {
+    ...productSample,
+    id: "2"
+  },
+  {
+    ...productSample,
+    id: "3"
+  },
+  {
+    ...productSample,
+    id: "4"
+  },
+  {
+    ...productSample,
+    id: "5"
+  }
+]
+
+const defaultArgs:Props = {
+  featuredProductGroups: [
+    {
+      title: "New Arrivals",
+      mainProduct: productSample,
+      otherProducts: productSamples
+    },
+    {
+      title: "Customer Favorites",
+      mainProduct: productSample,
+      otherProducts: productSamples
+    },
+  ]
+};
+
+
+
 export default {
   title: "Sections/FeaturedProductsSection",
   component: FeaturedProductsSection,
-  args: {}
+  args: defaultArgs
 } as Meta;
 
 
@@ -15,9 +80,5 @@ export default {
 type Story = StoryObj<typeof FeaturedProductsSection>;
 
 
-export const Default: Story = {
-  args: {
-
-  }
-};
+export const Default: Story = {};
 
--- src/components/sections/FeaturedProductsSection/index.tsx
+++ src/components/sections/FeaturedProductsSection/index.tsx
@@ -1,15 +1,33 @@
+import FeaturedProductGroup from "@/components/composites/FeaturedProductGroup";
+import List from "@/components/generics/List";
 import styles from "@styles/sections/FeaturedProductsSection.module.scss";
 
+import type {Props as FeaturedProductGroupProps} from "@components/composites/FeaturedProductGroup"; 
+
 export interface Props {
+    featuredProductGroups: FeaturedProductGroupProps[];
 }
 
 
-export default function FeaturedProductsSection({ }: Props) {
+export default function FeaturedProductsSection({ featuredProductGroups }: Props) {
+
+    const items = featuredProductGroups.map((group, index) => {
+        return ({
+            id: group.title,
+            ...group
+        });
+    });
 
 
     return (
-        <section className={styles.placeholder}>
-            FeaturedProducts Section
+        <section className={styles.wrapper}>
+            <List 
+                items = {items}
+                ItemCPN = {FeaturedProductGroup}
+                liClass = {styles.li} 
+                ulClass = {styles.ul}
+                horizontal
+            /> 
         </section>
     );
 }
