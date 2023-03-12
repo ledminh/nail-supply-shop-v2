cdfb06abf080efaa28e71295c3c5ad65d8de06b0
Minh Le <duyminh.le310@gmail.com>
2023-03-12 03:08:40 -0400

    Add InfoBlock component and styles to project

---
+++ src/components/generics/InfoBlock/index.tsx
@@ -0,0 +1,28 @@
+import SeparatorCPN from '@/components/basics/SeparatorCPN';
+import styles from '@styles/generics/InfoBlockCPN.module.scss';
+import { ReactNode } from 'react';
+
+
+export interface Props {
+    title: string;
+    children: ReactNode
+};
+
+export default function InfoBlock<T extends JSX.IntrinsicAttributes>({
+    title,
+    children    
+}: Props) {
+
+    
+
+    return (
+        <div className={styles.wrapper}>
+            <h4 className={styles.title}>{title}</h4>
+            <SeparatorCPN/>
+            <div className={styles.body}>
+                {children}
+            </div>
+        </div>
+
+    );
+}
