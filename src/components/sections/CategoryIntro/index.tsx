89d0f18b1ef83487e38f60f737d6865060984a07
Minh Le <duyminh.le310@gmail.com>
2023-03-12 01:45:32 -0500

    Add CategoryIntroSection component, styles, and stories

---
+++ src/components/sections/CategoryIntro/index.tsx
@@ -0,0 +1,32 @@
+import BannerCPN from "@/components/basics/BannerCPN";
+import CategoryList from "@/components/composites/CategoryList";
+import styles from "@styles/sections/CategoryIntroSection.module.scss";
+
+import type { Category } from "@/types/category";
+
+export interface Props {
+    categoryIntro: string;
+    categories: Category[];
+}
+
+
+export default function CategoryIntroSection({ categoryIntro, categories }: Props) {
+
+
+    return (
+        <section className={styles.wrapper}>
+            <div className={styles.banner}>
+                <BannerCPN 
+                    text={categoryIntro}
+                />
+            </div>
+            <div className={styles.list}>
+                <CategoryList 
+                    categories={categories}
+                />
+            </div>
+        </section>
+    );
+}
+
+CategoryIntroSection.displayName = "CategoryIntroSection";
