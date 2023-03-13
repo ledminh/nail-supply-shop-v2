1d2943c20c8ab9db545c69b93d718efa286db2bc
Minh Le <duyminh.le310@gmail.com>
2023-03-13 17:26:06 -0400

    Add styling for ButtonCPN

--- src/components/basics/ButtonCPN/index.tsx
+++ src/components/basics/ButtonCPN/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-
+import { MouseEventHandler } from "react";
 
 
 import styles from "@styles/basics/ButtonCPN.module.scss";
@@ -7,18 +7,28 @@ import styles from "@styles/basics/ButtonCPN.module.scss";
 
 
 export interface Props  {
-
+    type: "normal" | "attention" | "danger";
+    onClick?: MouseEventHandler<HTMLButtonElement> ;
+    icon?: {
+        position: "left" | "right";
+        node: JSX.Element;
+    };
+    label: string;
+    className?: string;
 };
 
 type Button = FC<Props>;
 
 
-const ButtonCPN:Button = ({}) => {
+const ButtonCPN:Button = ({type, onClick, icon, label, className}) => {
 
 
     return (
-        <button>
-            Button
+        <button className={type === "normal" ? styles.normal : type === "attention" ? styles.attention : styles.danger + (className? " " + className : "")}
+            onClick={onClick}>
+            {icon?.position === "left" && <span className={styles.icon}>icon.node</span>}
+            <span className={styles.label}>{label}</span>
+            {icon?.position === "right" && <span className={styles.icon}>icon.node</span>}
         </button>
     );
 }
