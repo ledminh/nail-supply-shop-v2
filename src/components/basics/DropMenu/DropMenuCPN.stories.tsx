dc879c0051320a74f51b6fbc1c765dc927968fa5
Minh Le <duyminh.le310@gmail.com>
2023-03-14 17:32:25 -0400

    Add logic for DropMenuCPN Story to handle onChange and display current label

--- src/components/basics/DropMenu/DropMenuCPN.stories.tsx
+++ src/components/basics/DropMenu/DropMenuCPN.stories.tsx
@@ -1,20 +1,54 @@
 import {  Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 
 import DropMenuCPN from '.';
 
 import type { Props } from '.';
 
-const defaultArgs:Props = {};
+const defaultArgs:Props = {
+    items: [
+      {
+        id: "001",
+        label: "Price",
+      },
+      {
+        id: "002",
+        label: "Name",
+      },
+      {
+        id: "003",
+        label: "Date",
+      },
+    ]
+
+};
 
 
-const Wrapper = (args:Props) => (
-  <div style={{ 
-    width: "400px",
-    border: "6px solid gray",
-    }}>
-    <DropMenuCPN {...args} />
-  </div>
-);
+const Wrapper = (args:Props) => {
+  const [currentLabel, setCurrentLabel] = useState(args.items[0].label);
+
+  const onChange = (id: string) => {
+    const item = args.items.find(item => item.id === id);
+    if (item) {
+      setCurrentLabel(item.label);
+    }
+  };
+
+  const argsWithOnChange = {
+    ...args,
+    onChange,
+  };
+
+  return (
+    <div style={{ 
+      width: "400px",
+      border: "6px solid gray",
+      }}>
+      <DropMenuCPN {...argsWithOnChange} />
+      <div>{currentLabel}</div>
+    </div>
+  );
+};
 
 export default {
   title: "Basics/DropMenuCPN",
--- src/components/basics/DropMenu/index.tsx
+++ src/components/basics/DropMenu/index.tsx
@@ -1,34 +1,69 @@
-import { FC, MouseEventHandler, useState} from "react";
+import { FC, useEffect, useState} from "react";
 
 import styles from "@styles/basics/DropMenuCPN.module.scss";
 import List from "@/components/generics/List";
 
 
-
+type ItemCPNProps = {
+    id: string;
+    label: string;
+}
 
 export interface Props  {
-
+    items: (ItemCPNProps & {id: string})[];
+    liClass?: string;
+    ulClass?: string;
+    activeLiClass?: string;
+    itemClass?: string;
+    onChange?: (id: string) => void;
 };
+
 
 type DropMenu = FC<Props>;
 
 
-const DropMenuCPN:DropMenu = ({}) => {
-    const [currentItemID, setCurrentItemID] = useState("Item 1");
+const DropMenuCPN:DropMenu = ({items, liClass, ulClass, activeLiClass, itemClass, onChange}) => {
+    const [currentItem, setCurrentItem] = useState(items[0]);
+
+    const itemOnClick = (id: string) => {
+        setCurrentItem(items.find(item => item.id === id) as ItemCPNProps);
+    }
+
+        
+    const ItemCPN = ({id, label}:ItemCPNProps) => {
+        return (
+            <div className={styles.item + (itemClass? ' ' + itemClass: '')}
+                onClick={e => {
+                    e.stopPropagation();
+                    itemOnClick(id);
+                }}
+                >
+                {label}
+            </div>
+        );
+    }
+
+
+    useEffect(() => {
+        onChange && onChange(currentItem.id);
+    }, [currentItem.id]);
 
     return (
         <div className={styles.wrapper}>
-            <div className={styles.header}></div>
+            <div className={styles.header}>
+                <span>{currentItem.label}</span>
+                    
+            </div>
             <div className={styles.body}>
                 <List 
-                    items 
+                    items = {items}
                     ItemCPN = {ItemCPN}
-                    liClass = {styles.li}
-                    ulClass = {styles.ul}
-                    activeItem = {{
-                        id: "Item 1",
-                        className: styles.activeItem,
-                    }}
+                    liClass = {styles.li + ' ' + (liClass? liClass: '')}
+                    ulClass = {styles.ul + ' ' + (ulClass? ulClass : '')}
+                    activeItem = {activeLiClass? {
+                        id: currentItem.id,
+                        className: activeLiClass ,
+                    }: undefined}
                 />
             </div>
         </div>
@@ -39,17 +74,3 @@ export default DropMenuCPN;
 
 DropMenuCPN.displayName = "DropMenuCPN";
 
-type ItemCPNProps = {
-    onClick: MouseEventHandler<HTMLButtonElement>;
-    label: string;
-}
-
-const ItemCPN = ({onClick, label}:ItemCPNProps) => {
-    return (
-        <button className={styles.item}
-            onClick={onClick}
-        >
-            {label}
-        </button>
-    );
-}
