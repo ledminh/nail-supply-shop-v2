7d03d180d37991aeefeb4376c2b2a5023fdfdc90
Minh Le <duyminh.le310@gmail.com>
2023-03-14 16:33:36 -0400

    Add List component for DropMenuCPN and ItemCPN for dropdown list items

--- src/components/basics/DropMenu/DropMenuCPN.stories.tsx
+++ src/components/basics/DropMenu/DropMenuCPN.stories.tsx
@@ -7,9 +7,18 @@ import type { Props } from '.';
 const defaultArgs:Props = {};
 
 
+const Wrapper = (args:Props) => (
+  <div style={{ 
+    width: "400px",
+    border: "6px solid gray",
+    }}>
+    <DropMenuCPN {...args} />
+  </div>
+);
+
 export default {
   title: "Basics/DropMenuCPN",
-  component: DropMenuCPN,
+  component: Wrapper,
   args: defaultArgs
 } as Meta;
 
--- src/components/basics/DropMenu/index.tsx
+++ src/components/basics/DropMenu/index.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react";
-
-
+import { FC, MouseEventHandler, useState} from "react";
 
 import styles from "@styles/basics/DropMenuCPN.module.scss";
+import List from "@/components/generics/List";
+
 
 
 
@@ -14,11 +14,23 @@ type DropMenu = FC<Props>;
 
 
 const DropMenuCPN:DropMenu = ({}) => {
-
+    const [currentItemID, setCurrentItemID] = useState("Item 1");
 
     return (
         <div className={styles.wrapper}>
-            
+            <div className={styles.header}></div>
+            <div className={styles.body}>
+                <List 
+                    items 
+                    ItemCPN = {ItemCPN}
+                    liClass = {styles.li}
+                    ulClass = {styles.ul}
+                    activeItem = {{
+                        id: "Item 1",
+                        className: styles.activeItem,
+                    }}
+                />
+            </div>
         </div>
     );
 }
@@ -26,3 +38,18 @@ const DropMenuCPN:DropMenu = ({}) => {
 export default DropMenuCPN;
 
 DropMenuCPN.displayName = "DropMenuCPN";
+
+type ItemCPNProps = {
+    onClick: MouseEventHandler<HTMLButtonElement>;
+    label: string;
+}
+
+const ItemCPN = ({onClick, label}:ItemCPNProps) => {
+    return (
+        <button className={styles.item}
+            onClick={onClick}
+        >
+            {label}
+        </button>
+    );
+}
