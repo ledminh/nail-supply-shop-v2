6b5358e8101e4d316bf9af5a608caca9acdfaf72
Minh Le <duyminh.le310@gmail.com>
2023-03-19 00:51:29 -0400

    Add Select component to TabLayout, update styles for TabLayout components and Select component

--- src/components/layouts/TabLayout/index.tsx
+++ src/components/layouts/TabLayout/index.tsx
@@ -1,6 +1,10 @@
 import React, { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+
 import Link from 'next/link';
 import styles from '@styles/layouts/TabLayout.module.scss';
+
+import Select, { convertToOptionItem } from '@/components/generics/Select';
 
 interface Tab {
     name: string;
@@ -16,6 +20,8 @@ interface TabLayoutProps {
 export default function TabLayout(props: TabLayoutProps) {
     const { tabs, currentTabSlug, children } = props;
 
+    const router = useRouter();
+
     const tabLinks = tabs.map((tab) => (
         <Link key={tab.slug} href={`/admin/${tab.slug}`}
             className={`${styles.tab} ${tab.slug === currentTabSlug ? styles.currentTab : ''}`}
@@ -24,10 +30,31 @@ export default function TabLayout(props: TabLayoutProps) {
         </Link>
     ));
 
+
+
     return (
         <div className={styles.wrapper}>
-            <div className={styles.tabs}>{tabLinks}</div>
+            <div className={styles.header}>
+                <div className={styles.tabs}>{tabLinks}</div>
+                <Select 
+                    selectClass = {styles.select}
+                    optionClass = {styles.option}
+                    optionItems = {tabs.map(convertTabToOption)}
+                    initOptionItem = {convertTabToOption(tabs.find((tab) => tab.slug === currentTabSlug)?? tabs[0])}
+                    onChange = {(optionItem) => router.push(`/admin/${optionItem.value}`)}
+                    />
+            </div>
             <div className={styles.content}>{children}</div>
         </div>
     );
+}
+
+
+TabLayout.displayName = 'TabLayout';
+
+const getValue = (tab: Tab) => tab.slug;
+const getLabel = (tab: Tab) => tab.name;
+
+const convertTabToOption = (tab: Tab) => {
+    return convertToOptionItem({item: tab, getValue, getLabel});
 }
