cd41e2da5c0ea07cee48e635c9f18f65fb5fbc01
Minh Le <duyminh.le310@gmail.com>
2023-03-20 00:19:06 -0400

    export const Default: StoryObj = (props) => <AdminCategoryBlockCPN {...defaultArgs} {...props} />Add click and delete functionality to AdminCategoryBlockCPN stories

--- src/components/basics/AdminCategoryBlock/AdminCategoryBlockCPN.stories.tsx
+++ src/components/basics/AdminCategoryBlock/AdminCategoryBlockCPN.stories.tsx
@@ -1,16 +1,58 @@
 import {  Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 
 import AdminCategoryBlockCPN from '.';
 
 import type { Props } from '.';
 
-const defaultArgs:Props = {};
+
+
+const defaultArgs = {
+  id: "1",
+  name: "Category Sample",
+  image: {
+    src: "https://loremflickr.com/300/300",
+    alt: "Category Image",
+  },
+  description: "This is a sample category for testing purposes. Don't take it seriously. It's just a sample. Another sample. Only another sample",
+} as Props;
+
+
+
+const Wrapper = () => {
+
+  const [onClickText, setOnClickText] = useState("");
+  const [onDeleteText, setOnDeleteText] = useState("");
+
+  const args = defaultArgs;
+
+  args.onClick = (catID: string) => {
+    setOnClickText(`Clicked on ${catID}`);
+  };
+
+  args.onDelete = (catID: string) => {
+    setOnDeleteText(`Deleted ${catID}`);
+  };
+
+
+
+  return (
+    <>
+      <AdminCategoryBlockCPN {...args} />
+      <p>{onClickText}</p>
+      <p>{onDeleteText}</p>
+    </>
+  )
+    
+};
+  
+
 
 
 export default {
   title: "Basics/AdminCategoryBlockCPN",
-  component: AdminCategoryBlockCPN,
-  args: defaultArgs
+  component: Wrapper,
+  args: {}
 } as Meta;
 
 
