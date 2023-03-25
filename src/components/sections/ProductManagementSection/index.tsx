f681b9a4b91dd610ad15e7f7a317280d2909bd76
Minh Le <duyminh.le310@gmail.com>
2023-03-25 03:26:55 -0400

    General outline of ProductManagementSection

--- src/components/sections/ProductManagementSection/index.tsx
+++ src/components/sections/ProductManagementSection/index.tsx
@@ -1,27 +1,10 @@
 import List from "@/components/generics/List";
 import styles from "@styles/sections/ProductManagementSection.module.scss";
-
 import { Product, ProductGroup } from "@/types/product";
-import {useState, useEffect} from "react";
 
-import WarningModal from "@/components/composites/WarningModal";
+import axios from "axios";
 
-import { ProductApiResponse } from "@/pages/api/products";
-
-import axios, { AxiosResponse } from "axios";
-
-import { RemoteImage } from "@/types/image";
-
-import { ProductImage } from "@/types/product";
-import ProductModal from "@/components/composites/ProductModal";
-
-import AdminProductBlockCPN, {Props as AdminProductBlockProps} from "@/components/basics/AdminProductBlock";
-import AdminProductGroupBlockCPN, {Props as AdminProductGroupBlockProps} from "@/components/basics/AdminProductGroupBlock";
-import useDelete from "@/hooks/ProductManagementSection/useDelete";
-
-import useEditProduct from "@hooks/ProductManagementSection/useEditProduct";
-
-import ProductGroupModal from "@/components/composites/ProductGroupModal";
+import { useState, useEffect } from "react";
 
 export interface Props {
 }
@@ -31,254 +14,37 @@ export default function ProductManagementSection({  }: Props) {
 
     const [products, setProducts] = useState<(Product|ProductGroup)[]>([]);
 
-    const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
-    const [warningMessage, setWarningMessage] = useState<string>('');
-
-    const [toBeDeletedProductID, setToBeDeletedProductID] = useState<string | null>(null);
-    const [toBeDeletedProductGroupID, setToBeDeletedProductGroupID] = useState<string | null>(null);
-
-    const [isProductModalOpen, setIsProductModalOpen] = useState(false);
-    const [productModalType, setProductModalType] = useState<'create' | 'edit'|null>(null);
-    const [beingEditedProduct, setBeingEditedProduct] = useState<Product | null>(null);
-    
-    const [isProductGroupModalOpen, setIsProductGroupModalOpen] = useState(false);
-    const [productGroupModalType, setProductGroupModalType] = useState<'create' | 'edit'|null>(null);
-    const [beingEditedProductGroup, setBeingEditedProductGroup] = useState<ProductGroup | null>(null);
-
-
-    const { onDeleteProduct, onDeleteProductGroup, deleteProduct, deleteProductGroup } =  useDelete({ products, setProducts,         setIsWarningModalOpen, setWarningMessage, setToBeDeletedProductID,setToBeDeletedProductGroupID});
-
-    const {updateProduct, onEditProduct} = useEditProduct({products, setProducts, setIsProductModalOpen, setProductModalType, setBeingEditedProduct, setIsProductGroupModalOpen, beingEditedProductGroup});
-
-
     useEffect(() => {
-        axios.get('/api/products')
-            .then((res:AxiosResponse<ProductApiResponse>) => {
-                setProducts(res.data as (Product|ProductGroup)[]);
-
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-
+        loadProducts().then((products) => setProducts(products));
     }, []);
-
-    /********************************
-     * Functions for ProductModal
-     */
     
-    const onEditProductGroup = (prodID: string) => {
-        setProductGroupModalType('edit');
-        setBeingEditedProductGroup(products.find((prod) => prod.id === prodID) as ProductGroup);
-        setIsProductGroupModalOpen(true);
-    }
-
-    const onCreateProduct = () => {
-        // setCategoryModalType('create');
-        // setIsCategoryModalOpen(true);
-
-    }
-
     
 
-
-    const createNewCategory = (name: string, description: string, image: File) => {
-        // // Upload the image first
-        // const imageFormData = new FormData();
-        // imageFormData.append('cat-image', image);
-
-        // axios.post('/api/upload?type=cat-image', imageFormData, {
-        //     headers: {
-        //         'Content-Type': 'multipart/form-data'
-        //     }
-        // }).then((res) => res.data)
-        // .then((imageData) => {
-            
-        //     const formData = new FormData();
-
-        //     formData.append('name', name);
-        //     formData.append('description', description);
-        //     formData.append('imageFileName', imageData.filename);
-
-        //     return axios.post('/api/categories?type=create', formData);
-        // }).then((res) => res.data)
-        // .then((data) => {
-
-        //     setCategories([data, ...categories]);
-        // })
-        // .catch((err) => {
-        //     console.error(err);
-        // });
-    };
-
-
-
-    const convertToProductItem = (product: Product | ProductGroup) => {
-        if(isProduct(product)){ // product is a Product
-            return {
-                id: product.id,
-                onDelete: onDeleteProduct,
-                onClick: onEditProduct,
-                product
-            }
-        }
-        else { // product is a ProductGroup
-            return {
-                id: product.id,
-                onDelete: onDeleteProductGroup,
-                onClick: onEditProductGroup,
-                onEditProduct: onEditProduct,
-                productGroup: product
-            }
-        }
-    
-    }
 
     return (
-        <>
-            <section className={styles.wrapper}>
-                <List 
-                    items = {products.map(convertToProductItem)}
-                    ItemCPN = {ItemCPN}
-                    liClass = {styles.li}
-                    ulClass = {styles.ul}
-                />
-            </section>
-            {
-                isProductModalOpen &&  (
-                    productModalType === 'edit'?
-                        <ProductModal type='edit' 
-                            onSave={({serialNumber, name, intro, details, price, images}) => {
-                                updateProduct(beingEditedProduct!.id, serialNumber,name, intro, details, price, images);
-                                setIsProductModalOpen(false);
-                                setProductModalType(null);
-                                setBeingEditedProduct(null);
+        <section className={styles.wrapper}>
+            <List 
+                items = {products}
+                ItemCPN = {(product) => <>{product.name}</>}
+                liClass = {styles.li}
+                ulClass = {styles.ul}
+            />
+        </section>
+    )
 
-                                if(beingEditedProductGroup){
-                                    setIsProductGroupModalOpen(true);
-                                }
-                            }} 
-                            onCancel={() => {
-                                setIsProductModalOpen(false);
-                                setProductModalType(null);
-                                setBeingEditedProduct(null);
-
-                                if(beingEditedProductGroup){
-                                    setIsProductGroupModalOpen(true);
-                                }
-                            }}
-                            initName = {beingEditedProduct?.name || ''} 
-                            initIntro = {beingEditedProduct?.intro || ''}
-                            initDetails = {beingEditedProduct?.details || ''}
-                            initPrice = {beingEditedProduct?.price || 0}
-                            initSerialNumber = {beingEditedProduct?.id || ''} 
-                            initImages = {beingEditedProduct?.images || []} 
-                            /> : null
-                    // <ProductModal type='create' 
-                    //     onSave={({name, description, image}) => {
-                            
-                    //         createNewCategory(name, description, image as File);
-                    //         setIsCategoryModalOpen(false);
-                    //         setCategoryModalType(null);
-                    //     }} 
-                    //     onCancel={() => {
-                    //         setIsCategoryModalOpen(false);
-                    //         setCategoryModalType(null);
-                    //     }}
-                    //     /> : 
-                )
-            }
-            {
-                isProductGroupModalOpen &&  (
-                    productGroupModalType === 'edit'?
-                        <ProductGroupModal type = "edit" 
-                            onSave = {()=>{}} 
-                            onCancel = {()=>{}} 
-                            initName = {beingEditedProductGroup?.name || ''} 
-                            initProducts = {beingEditedProductGroup?.products || []}   
-                            /> : null)
-            }
-            {
-                isWarningModalOpen && (
-                    <WarningModal message={warningMessage}
-                        onOK={() => {
-                            if(toBeDeletedProductID)
-                                deleteProduct(toBeDeletedProductID);
-                            else if(toBeDeletedProductGroupID)
-                                deleteProductGroup(toBeDeletedProductGroupID);
-
-                            setIsWarningModalOpen(false);
-                        }} 
-                        onCancel={() => {
-                            setToBeDeletedProductID(null);
-                            setIsWarningModalOpen(false);
-                        }}
-                        />)
-            }
-        </>
-    );
 }
 
 ProductManagementSection.displayName = "ProductManagementSection";
 
 
-
-
-interface ItemCPNProps {
-    onDelete: (id: string) => void;
-    onClick: (id: string) => void;
-    product?: Product;
-    productGroup?: ProductGroup;
-    onEditProduct?: (productID: string) => void;
-
-}
-
-const ItemCPN = ({
-    onDelete,
-    onClick,
-    onEditProduct,
-    product,
-    productGroup,
-    
-}: ItemCPNProps) => {
-    if (product) {
-        const adminProductProps: AdminProductBlockProps = {
-            ...product,
-            onDelete,
-            onClick,
-        };
-        return (
-            <AdminProductBlockCPN
-                {...adminProductProps}
-                className={styles.productBlock}
-            />
-        );
-    } else if (productGroup && onEditProduct) {
-        const adminProductGroupProps: AdminProductGroupBlockProps = {
-            ...productGroup,
-            onDelete,
-            onClick,
-            onEditProduct
-        };
-        return (
-            <AdminProductGroupBlockCPN
-                {...adminProductGroupProps}
-                className={styles.productBlock}
-            />
-        );
-    } else {
-        return null;
-    }
-};
-
-
-
-/*************************
- * Helpers
+/****************************
+ * Helper functions
  */
 
-// check if product is Product or ProductGroup
-const isProduct = (product: Product | ProductGroup): product is Product => {
-    return (product as Product).price !== undefined;
+function loadProducts(): Promise<(Product|ProductGroup)[]> {
+    return axios.get("/api/products")
+        .then((res) => res.data)
+        .catch((err) => {
+            throw err;
+        });
 }
-
