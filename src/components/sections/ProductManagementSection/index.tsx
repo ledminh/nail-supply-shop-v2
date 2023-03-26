35d073d3a826d051eda299c95442b56be5864854
Minh Le <duyminh.le310@gmail.com>
2023-03-25 22:07:19 -0400

    processImage

--- src/components/sections/ProductManagementSection/index.tsx
+++ src/components/sections/ProductManagementSection/index.tsx
@@ -12,6 +12,8 @@ import AdminProductGroupBlockCPN from "@/components/basics/AdminProductGroupBloc
 import { productManagementConfig } from "@/config";
 import { useProductModal } from "@/components/composites/ProductModal";
 
+import { ProductImage } from "@/types/product";
+
 const {warningMessages} = productManagementConfig;
 
 export interface Props {
@@ -45,16 +47,14 @@ export default function ProductManagementSection({  }: Props) {
         openEditProduct({
             product,
             onSave: ({serialNumber, name, intro, details, price, images}) => {
-                const formData = new FormData();
-
+                const formData = createFormData({serialNumber, name, intro, details, price});
                 
-
-                formData.append("serialNumber", serialNumber);
-                formData.append("name", name);
-                formData.append("intro", intro);
-                formData.append("details", details);
-                formData.append("price", price.toString());
-                
+                processImages(images).then((images) => {
+                    images.forEach((image) => {
+                        formData.append("images", image);
+                    });
+                    console.log(formData);
+                });
                 
 
             },
@@ -219,4 +219,61 @@ function useDelete ({products, setProducts, showWarning}:useDeleteProps) {
         onDeleteProduct,
         onDeleteGroup
     }
+}
+
+const processImages = async (images: (File | ProductImage)[]):Promise<ProductImage[]> => {
+    // images can be File[], ProductImage[] or a mixture of both
+    // if images are File[], upload them to server, get the filenames, and create ProductImage[]
+    // if images are RemoteImage[], just return them
+
+    const files = images.filter((image) => image instanceof File) as File[];
+    const productImages = images.filter(
+        (image) => !(image instanceof File)
+    ) as ProductImage[];
+
+    if (files.length === 0) {
+        return Promise.resolve(productImages);
+    } else {
+        const res = await uploadProductImages(files);
+        const filenames = res.data.filenames as string[];
+        const newProductImages = filenames.map(
+            (filename) => ({
+                // generate a unique id string for each image with Date.now() and Math.random()
+                id: `${Date.now()}-${Math.random()}`,
+                src: `/images/product/${filename}`,
+                alt: filename,
+            })
+        );
+        return [...productImages, ...newProductImages];
+    }
+
+}
+
+const uploadProductImages = (images: File[]) => {
+    const imageFormData = new FormData();
+
+    
+    images.forEach((image) => {
+        imageFormData.append('product-images', image);
+    });
+
+    return axios.post('/api/upload?type=product-images', imageFormData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    });
+}
+
+
+
+function createFormData(obj: any) {
+    const formData = new FormData();
+
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            formData.append(key, obj[key].toString());
+        }
+    }
+
+    return formData;
 }
