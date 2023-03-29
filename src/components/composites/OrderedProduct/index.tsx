f6fd4dee0364c01dfef26ebe4b35b8b1cdb781ea
Minh Le <duyminh.le310@gmail.com>
2023-03-29 01:13:40 -0400

    Commit: Add onChange handler to OrderedProduct component, calculate and display total price

--- src/components/composites/OrderedProduct/index.tsx
+++ src/components/composites/OrderedProduct/index.tsx
@@ -1,13 +1,14 @@
 import ImageCPN from "@/components/basics/ImageCPN";
 import QuantityPickerCPN from "@/components/basics/QuantityPicker";
-import { ProductImage } from "@/types/product";
+import {  OrderedProduct as OrderedProductType, ProductImage } from "@/types/product";
 import styles from "@styles/composites/OrderedProduct.module.scss";
 
 import removeIcon from "@images/cancel_icon.svg";
 import { LocalImage } from "@/types/image";
 
-export interface Props {
-}
+import { useState, useEffect } from "react";
+
+
 
 
 const sampleImg:ProductImage = {
@@ -21,28 +22,40 @@ const removeIconImage:LocalImage = {
     alt: "cancel icon",
 }
 
-export default function OrderedProduct({ }: Props) {
+export interface Props extends OrderedProductType {
+    onChange: ({id, quantity}: {id:string, quantity:number}) => void;
+}
 
+export default function OrderedProduct({ id, name, price, quantity, image, onChange }: Props) {
+
+    const [totalPrice, setTotalPrice] = useState(price*quantity);
+
+
+    useEffect(()=> {
+        setTotalPrice(price*quantity);
+    }, [quantity])
 
     return (
         <div className={styles.wrapper}>
             <QuantityPickerCPN
-                value = {2}
-                onChange = {() => {}}
+                value = {quantity}
+                onChange = {(value) => {
+                    onChange({id, quantity: value});
+                }}
                 buttonClassName = {styles.quantityButton}
                 valueClassName = {styles.quantityValue}
                 className = {styles.quantityPicker}
             />
             <ImageCPN
-                image = {sampleImg}
+                image = {image}
                 size = "medium"
                 className = {styles.productImage}
             />
             <div className={styles.info}>
-                <h3 className={styles.productName}>Product Name</h3>
-                <p className={styles.price}>100</p>
+                <h3 className={styles.productName}>{name}</h3>
+                <p className={styles.price}>{price}</p>
             </div>
-            <div className={styles.totalPrice}>300</div>
+            <div className={styles.totalPrice}>{totalPrice}</div>
             <button className={styles.removeButton}>
                 <ImageCPN
                     image = {removeIconImage}
